cafd1a9c7681ab6a948708236691854e4bfd1ec4
jar.git <jar.x64@gmail.com>
2018-03-24 23:16:12 +0200

    Added toolchain file for clang and fixed clang to use llvm-lld instead of gold linker.

---
+++ config/clang-toolchain.cmake
@@ -0,0 +1,22 @@
+set(CMAKE_SYSTEM_NAME ${CMAKE_HOST_SYSTEM_NAME})
+set(CMAKE_SYSTEM_PROCESSOR ${CMAKE_HOST_SYSTEM_PROCESSOR})
+# set(CMAKE_SYSROOT /usr)
+
+# See: https://clang.llvm.org/docs/CrossCompilation.html
+# set(clang-options unknown)
+
+set(CMAKE_C_COMPILER clang CACHE sring "Clang C compiler" FORCE)
+# set(CMAKE_C_COMPILER_TARGET ${clang-options})
+set(CMAKE_CXX_COMPILER clang++ CACHE string "Clang C++ compiler" FORCE)
+# set(CMAKE_CXX_COMPILER_TARGET ${clang-options})
+
+# WTF?
+set(THREADS_PTHREAD_ARG "ON" CACHE string "whye, o whye." FORCE)
+
+# set(CMAKE_AR      "/usr/bin/llvm-ar")
+# https://lld.llvm.org/
+# set(CMAKE_LINKER  "/usr/bin/llvm-ld")
+# set(CMAKE_NM      "/usr/bin/llvm-nm")
+# set(CMAKE_OBJDUMP "/usr/bin/llvm-objdump")
+# set(CMAKE_RANLIB  "/usr/bin/llvm-ranlib")
+
