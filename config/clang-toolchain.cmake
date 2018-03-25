3cbc9a6e19e364347899f673b9e940349432fb10
jar.git <jar.x64@gmail.com>
2018-03-25 11:31:50 +0300

    Fixed static lib linking with clang toolchain.

--- config/clang-toolchain.cmake
+++ config/clang-toolchain.cmake
@@ -13,10 +13,10 @@ set(CMAKE_CXX_COMPILER clang++ CACHE string "Clang C++ compiler" FORCE)
 # Needs to be set when cross compiling.
 set(THREADS_PTHREAD_ARG "ON" CACHE string "Don't try." FORCE)
 
-# set(CMAKE_AR      "/usr/bin/llvm-ar")
+set(CMAKE_AR      "/usr/bin/llvm-ar")
 # https://lld.llvm.org/
-# set(CMAKE_LINKER  "/usr/bin/llvm-ld")
-# set(CMAKE_NM      "/usr/bin/llvm-nm")
-# set(CMAKE_OBJDUMP "/usr/bin/llvm-objdump")
-# set(CMAKE_RANLIB  "/usr/bin/llvm-ranlib")
+set(CMAKE_LINKER  "/usr/bin/llvm-ld")
+set(CMAKE_NM      "/usr/bin/llvm-nm")
+set(CMAKE_OBJDUMP "/usr/bin/llvm-objdump")
+set(CMAKE_RANLIB  "/usr/bin/llvm-ranlib")
 
