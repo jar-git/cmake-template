7e2590611e256bffb095f1410457088f1574fff3
jar.git <jar.x64@gmail.com>
2018-03-25 20:46:44 +0300

    Added comments for link time optimization.

--- config/clang-toolchain.cmake
+++ config/clang-toolchain.cmake
@@ -1,22 +1,29 @@
+################################################################################
+# Clang tool-chain for host system.
+################################################################################
+
+# Set host system and processor.
 set(CMAKE_SYSTEM_NAME ${CMAKE_HOST_SYSTEM_NAME})
 set(CMAKE_SYSTEM_PROCESSOR ${CMAKE_HOST_SYSTEM_PROCESSOR})
-# set(CMAKE_SYSROOT /usr)
 
-# See: https://clang.llvm.org/docs/CrossCompilation.html
+# Set system root directory.
+# set(CMAKE_SYSROOT /)
+
+# Clang cross compilation: https://clang.llvm.org/docs/CrossCompilation.html
 # set(clang-options unknown)
-
-set(CMAKE_C_COMPILER clang CACHE sring "Clang C compiler" FORCE)
 # set(CMAKE_C_COMPILER_TARGET ${clang-options})
-set(CMAKE_CXX_COMPILER clang++ CACHE string "Clang C++ compiler" FORCE)
 # set(CMAKE_CXX_COMPILER_TARGET ${clang-options})
 
-# Needs to be set when cross compiling.
+# Set C and CXX compilers.
+set(CMAKE_C_COMPILER clang CACHE sring "Clang C compiler" FORCE)
+set(CMAKE_CXX_COMPILER clang++ CACHE string "Clang C++ compiler" FORCE)
+
+# TRY-RUN is not supported for cross compiling, so disable it.
 set(THREADS_PTHREAD_ARG "ON" CACHE string "Don't try." FORCE)
 
+# Set linkers and other build tools.
 set(CMAKE_AR      "/usr/bin/llvm-ar")
-# https://lld.llvm.org/
 set(CMAKE_LINKER  "/usr/bin/llvm-ld")
 set(CMAKE_NM      "/usr/bin/llvm-nm")
 set(CMAKE_OBJDUMP "/usr/bin/llvm-objdump")
 set(CMAKE_RANLIB  "/usr/bin/llvm-ranlib")
-
--- tools/cmake/FindGold.cmake
+++ tools/cmake/FindGold.cmake
@@ -1,7 +1,11 @@
+################################################################################
+# Link time optimization.
+################################################################################
+
 option(gold "Enable gold linker." ON)
 
-# Define a function that will set gnu gold on when called if it exists. See:
-# https://cmake.org/cmake/help/v3.5/command/function.html
+# Defines a function that will set gnu gold on when called if it exists. Function:
+# https://cmake.org/cmake/help/v3.6/command/function.html
 function(enable_gnu_gold LD_VERSION)
     if ("${LD_VERSION}" MATCHES "GNU gold")
         set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -fuse-ld=gold -Wl,--disable-new-dtags")
@@ -14,18 +18,22 @@ endfunction()
 
 if(gold)
     if(CMAKE_COMPILER_IS_GNUCXX)
-        # Check the if gold is availalbe with execute_process. See:
-        # https://cmake.org/cmake/help/v3.5/command/execute_process.html
+        # Check the if gold is availalbe with execute_process. Command:
+        # https://cmake.org/cmake/help/v3.6/command/execute_process.html
         execute_process(COMMAND ${CMAKE_CXX_COMPILER} -fuse-ld=gold -Wl,--version ERROR_QUIET OUTPUT_VARIABLE LD_VERSION_OUT)
         enable_gnu_gold(${LD_VERSION_OUT})
     elseif(CMAKE_COMPILER_IS_GNUCC)
         execute_process(COMMAND ${CMAKE_C_COMPILER} -fuse-ld=gold -Wl,--version ERROR_QUIET OUTPUT_VARIABLE LD_VERSION_OUT)
         enable_gnu_gold(${LD_VERSION_OUT})
     elseif(CMAKE_CXX_COMPILER_ID MATCHES "Clang")
+        # Clang is using the LLVM Linker instead of the LLVM gold plugin. This is
+        # because the LLVM linker is faster. Linker and plugin:
+        # The LLVM-Linker     : https://lld.llvm.org/
+        # The LLVM gold plugin: https://llvm.org/docs/GoldPlugin.html
         set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -fuse-ld=lld")
 	    set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} -fuse-ld=lld")
     elseif(MSVC)
         # Microsoft Compiler.
+        message(FATAL_ERROR "MSVC is not supported yet")
     endif()
 endif()
-
