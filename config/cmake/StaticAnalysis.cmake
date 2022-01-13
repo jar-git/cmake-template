f19f2064e6e2bf27212d84165ecf6a8e9f614bd4
Jani Arola <jani.arola@protonmail.com>
2022-01-13 10:59:59 +0200

    Temporarily disable IWYU, cppclean and cppcheck

--- config/cmake/StaticAnalysis.cmake
+++ config/cmake/StaticAnalysis.cmake
@@ -30,8 +30,8 @@ function(add_static_analysis TARGET)
         # Disable globally enabled analysers for now.
         enable_clang_tidy(${TARGET})
         #enable_iwyu(${TARGET})
-        enable_cppcheck(${TARGET})
-        enable_cppclean(${PROJECT_NAME})
+        #enable_cppcheck(${TARGET})
+        #enable_cppclean(${PROJECT_NAME})
 
     else(NOT ${IS_IMPORTED} AND NOT ${TARGET_TYPE} STREQUAL "INTERFACE_LIBRARY")
 
